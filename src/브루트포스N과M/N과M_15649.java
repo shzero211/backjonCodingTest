657b23ab1648e0ad4646ec9d173300805614bc8b
sanghun <sanghun@gmail.com>
2022-03-07 23:58:47 +0900

    브루트포스N과M

---
+++ src/브루트포스N과M/N과M_15649.java
@@ -0,0 +1,16 @@
+package 브루트포스N과M;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N과M_15649 {
+public static void main(String[] args) throws IOException {
+	
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st=new StringTokenizer(br.readLine());
+	int n=Integer.parseInt(st.nextToken());
+	int m=Integer.parseInt(st.nextToken());
+}
+}
